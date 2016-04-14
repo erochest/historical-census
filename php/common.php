1e291d1803469936ed1b2986ae59f4241475d913
Eric Rochester <erochest@virginia.edu>
2016-04-14 15:21:47 -0400

    Cleaning up, getting running on recent PHP.

--- php/common.php
+++ php/common.php
@@ -1,19 +1,88 @@
 <?php
 /* Database connection information.  Called by each script that needs to connect to the database. */
-$hostName="***REMOVED***";
-$userName="***REMOVED***";
-$password="***REMOVED***";
-$dbName="cmm2t_CensusBrowser";
+$hostName = getenv('MYSQL_HOST');
+$userName = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbName   = getenv('MYSQL_DBNAME');
 
-$link = mysql_connect($hostName, $userName, $password) or die("Unable to connect to host $hostName");
+$link = mysqli_connect($hostName, $userName, $password, $dbName)
+    or die("Unable to connect to host $dbName@$hostName");
 
-mysql_select_db($dbName, $link) or die("Unable to select database $dbName");
+function query_fetch_all($link, $query) {
+    $result = mysqli_query($link, $query);
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+function query_fetch_one($link, $query) {
+    $result = mysqli_query($link, $query);
+    return $result->fetch_assoc();
+}
+
+function get_groups($link) {
+    $query = "Select * from Groups";
+    $data  = query_fetch_all($link, $query);
+    $grp   = array();
+
+    foreach ($data as $row) {
+        $grpid       = $row["groupid"];
+        $groupname   = $row["groupname"];
+        $grp[$grpid] = $groupname;
+    }
+
+    return $grp;
+}
 
 /**
  * Imports GET/POST/Cookie variables into the global scope
  */
-import_request_variables ("gpc");
-if (gettype($vars) == 'string') {
-  $vars = array($vars);
+function array_get($key, $array) {
+    if (array_key_exists($key, $array)) {
+        return $array[$key];
+    } else {
+        return null;
+    }
 }
+
+function validate_regex($value, $re, $name) {
+    if (!empty($value) && preg_match($re, $value) === 0) {
+        trigger_error("Invalid $name value: '$value'");
+        exit;
+    }
+}
+
+function validate_array($value, $re, $name) {
+    if (empty($value)) {
+        return array();
+    }
+    if (!is_array($value)) {
+        $value = array($value);
+    }
+
+    foreach ($value as $v) {
+        validate_regex($v, $re, $name);
+    }
+
+    return $value;
+}
+
+$input_vars = $_GET + $_POST;
+$denominator = array_get('denominator', $input_vars);
+$direction   = array_get('direction',   $input_vars);
+$numerator   = array_get('numerator',   $input_vars);
+$sort        = array_get('sort',        $input_vars);
+$stateid     = array_get('stateid',     $input_vars);
+$subject     = array_get('subject',     $_GET);
+$table       = array_get('table',       $input_vars);
+$vars        = array_get('vars',        $input_vars);
+$year        = array_get('year',        $input_vars);
+
+$var_valid   = '/^var\d+$/';
+$numerator   = validate_array($numerator   , $var_valid , 'numerator');
+$stateid     = validate_array($stateid     , '/^\d+$/'  , 'stateid');
+$vars        = validate_array($vars        , $var_valid , 'var');
+validate_regex($denominator , $var_valid      , 'denominator');
+validate_regex($direction   , '/ASC|DESC/'    , 'direction');
+validate_regex($sort        , '/^\w+$/'       , 'sort');
+validate_regex($table       , '/^\d\d\d\d$/'  , 'table');
+validate_regex($year        , '/^V\d\d\d\d$/' , 'year' );
 
--- php/start.php
+++ php/start.php
@@ -2,9 +2,9 @@
 require ("common.php");
 
 ## prints out the web template
-print_template($year);
+print_template($link, $year);
 
-function print_template ($year) {
+function print_template($link, $year) {
         $handle=fopen ("script_template.html","rb");
         $contents="";
         do {
@@ -15,77 +15,72 @@ function print_template ($year) {
                 $contents .=$data;
         } while (true);
         fclose($handle);
-        $contents=str_replace("<!-- title here -->", "Historical Census Browser", $contents);
+        $contents = str_replace(
+            "<!-- title here -->", "Historical Census Browser", $contents
+        );
         $pos = strpos($contents, "<!--  content here -->");
         //We don't need any stinking regular expressions!!!!!
         print substr($contents,0,$pos);
-        printbody($year);
+        printbody($link, $year);
         print substr($contents,$pos+strlen("<!-- content here -->")+1);
 }
 ## main body of the script--you must pass the same vars to both print_template and printbody
-function printbody($year){
-$yr=explode('V', $year);
-print "<h2>Census Data for Year $yr[1]</h2>\n"; 
-print "<p>The selection lists below represent all of the questions from the $yr[1] census recorded by ";
-print "<a href=\"http://www.icpsr.umich.edu/\">ICPSR</a>.  They have been grouped into categories for
-convenience. The categories are not part of the original data.</p>\n";
-print "<h3>Variables for $yr[1] Census</h3>\n";
-$query2="Select * from Groups";
-$result2=mysql_query($query2);
-$number2=mysql_numrows($result2);
-for ($i=0; $i<$number2; $i++) {
-	$grpid=mysql_result($result2, $i, "groupid");
-	$groupname=mysql_result($result2, $i, "groupname");
-	$grp[$grpid]=$groupname;
-}
-print "<p>";
-$query2="Select Distinct groupid from $year where groupid!=0 and groupid!=32 order by groupid";
-$result2=mysql_query($query2); 
-$number2=mysql_numrows($result2);
-for ($i=0; $i<$number2; $i++) {
-	$groupid=mysql_result($result2, $i, "groupid");
-	if ($groupid==33) {}
-	else { 
-		print "<a href=\"#$groupid\">$grp[$groupid]</a> &nbsp &nbsp"; 
-	}
-}
-print "</p>\n";
-print "<form action=\"state.php\" method=\"POST\">\n";
-print "<input type=\"hidden\" name=\"table\" value=\"$yr[1]\">\n";
-$first=0;
-$query="Select * from $year where groupid!=0 and groupid!=32 order by groupid";
-$result=mysql_query($query);
-$number=mysql_numrows($result);
-for ($i=0; $i<$number; $i++) {
-	$varname=mysql_result($result, $i, "varname");
-	$label=mysql_result($result, $i, "label");
-	$groupid=mysql_result($result, $i, "groupid");
-	if ($test==$groupid) { }
-	else { 
-		if ($first==0) { $first++; }
-		else {
-			print "</select></p>\n";
-		}
-		if ($groupid==33) {}
-		else { print "<a name=\"$groupid\"><h4>$grp[$groupid]</h4></a>\n"; }
-        	print "<p><select name=\"vars[]\" size=\"7\" multiple>\n"; 
-		$test=$groupid;
-	}
-	switch ($groupid){
-		case 32:
-			break;
-		case 0:
-			break;
-		default:
-			$l=strtolower($label);
-			print "<option value=\"$varname\">$l</option>\n"; 
-			break;
-	}
-}
-print "</select></p>\n<p>
-<input type=\"submit\"> &nbsp <input type=\"reset\">
-</form></p>\n";
+function printbody($link, $year){
+    $yr=explode('V', $year);
+    print "<h2>Census Data for Year $yr[1]</h2>\n";
+    print "<p>The selection lists below represent all of the questions from the $yr[1] census recorded by ";
+    print "<a href=\"http://www.icpsr.umich.edu/\">ICPSR</a>.  They have been grouped into categories for
+        convenience. The categories are not part of the original data.</p>\n";
+    print "<h3>Variables for $yr[1] Census</h3>\n";
+    $grp = get_groups($link);
+    print "<p>";
+    $query2 = "Select Distinct groupid from $year "
+        . "where groupid!=0 and groupid!=32 and groupid!=33 "
+        . "order by groupid";
+    $data2  = query_fetch_all($link, $query2);
+    for ($i=0; $i<count($data2); $i++) {
+        $groupid = $data2[$i]["groupid"];
+        print "<a href=\"#$groupid\">${grp[$groupid]}</a> &nbsp &nbsp";
+    }
+    print "</p>\n";
+    print "<form action=\"state.php\" method=\"POST\">\n";
+    print "<input type=\"hidden\" name=\"table\" value=\"$yr[1]\">\n";
+    $first = 0;
+    $query = "Select * from $year where groupid!=0 and groupid!=32 "
+        . "order by groupid";
+    $data  = query_fetch_all($link, $query);
+    $test  = -1;
+    for ($i=0; $i<count($data); $i++) {
+        $varname = $data[$i]["varname"];
+        $label   = $data[$i]["label"  ];
+        $groupid = $data[$i]["groupid"];
+        if ($test !== $groupid) {
+            if ($first !== 0) {
+                $first++;
+            } else {
+                print "</select></p>\n";
+            }
+            if ($groupid !== 33) {
+                print "<a name=\"$groupid\"><h4>${grp[$groupid]}</h4></a>\n";
+            }
+            print "<p><select name=\"vars[]\" size=\"7\" multiple>\n";
+            $test = $groupid;
+        }
+        switch ($groupid){
+        case 32:
+            break;
+        case 0:
+            break;
+        default:
+            $l=strtolower($label);
+            print "<option value=\"$varname\">$l</option>\n";
+            break;
+        }
+    }
+    print "</select></p>\n<p>
+        <input type=\"submit\"> &nbsp <input type=\"reset\">
+        </form></p>\n";
 
-mysql_close();
+    mysqli_close($link);
 }
 
