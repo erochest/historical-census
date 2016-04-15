3e24435bfddc5b8df3d71fb252f072715cb4f6a5
Eric Rochester <erochest@virginia.edu>
2016-04-14 21:31:48 -0400

    Cleaned up some files.

--- php/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-print "<html><head><meta http-equiv=\"Refresh\" CONTENT=\".5;
-URL=http://fisher.lib.virginia.edu/collections/stats/histcensus/php/start.php?year=V1790\">";
-print "<title>Historical Census Browser</title></head>";
-print "<body>Creating Map . . .<script src='/libtools.js' type='text/javascript'></script></body></html>";
